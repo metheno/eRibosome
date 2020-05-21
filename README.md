1320e874655c34715e7bdc2a855d72b24bf32a83
Qinglin Kong <metheno@users.noreply.github.com>
2020-05-21 12:42:38 +0800

    identification of shine-dalgarno seq

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # eRibosome
 
-Translate DNA sequence to polypeptide sequence.
+Translate DNA/mRNA sequence to polypeptide sequence.
+
+## Usage
+
+```bash
+$ cd /path/to/eRibosome
+$ bash eRibosome.sh
+```
--- eRibosome.sh
+++ eRibosome.sh
@@ -6,7 +6,7 @@
 #======================================
 
 PROGNAME=$0
-PROGVER=0.1.1
+PROGVER=0.1.2
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -16,51 +16,66 @@ plain='\033[0m'
 
 DNA_SEQ=""
 
-tabs 6
-
 declare -a CODON=(ATA ATC ATT ATG ACA ACC ACG ACT AAC AAT AAA AAG AGC AGT AGA AGG CTA CTC CTG CTT CCA CCC CCG CCT CAC CAT CAA CAG CGA CGC CGG CGT GTA GTC GTG GTT GCA GCC GCG GCT GAC GAT GAA GAG GGA GGC GGG GGT TCA TCC TCG TCT TTC TTT TTA TTG TAC TAT TAA TAG TGC TGT TGA TGG)
 declare -a AMINO=(I-Ile I-Ile I-Ile ${green}M-Met${plain} T-Thr T-Thr T-Thr T-Thr N-Asn N-Asn K-Lys K-Lys S-Ser S-Ser R-Arg R-Arg L-Leu L-Leu L-Leu L-Leu P-Pro P-Pro P-Pro P-Pro H-His H-His Q-Gln Q-Gln R-Arg R-Arg R-Arg R-Arg V-Val V-Val V-Val V-Val A-Ala A-Ala A-Ala A-Ala D-Asp D-Asp E-Glu E-Glu G-Gly G-Gly G-Gly G-Gly S-Ser S-Ser S-Ser S-Ser F-Phe F-Phe L-Leu L-Leu Y-Tyr Y-Tyr ${red}-----${plain} ${red}-----${plain} C-Cys C-Cys ${red}-----${plain} W-Trp)
 declare -a PROPT=(NON-POLAR NON-POLAR NON-POLAR NON-POLAR ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${green}+1${plain} ${green}+1${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${green}+1${plain} ${green}+1${plain} NON-POLAR NON-POLAR NON-POLAR NON-POLAR SPECIAL SPECIAL SPECIAL SPECIAL ${green}+1${plain} ${green}+1${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${green}+1${plain} ${green}+1${plain} ${green}+1${plain} ${green}+1${plain} NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR ${red}-1${plain} ${red}-1${plain} ${red}-1${plain} ${red}-1${plain} SPECIAL SPECIAL SPECIAL SPECIAL ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} ${yellow}POLAR${plain} NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR NON-POLAR STOP STOP SPECIAL SPECIAL STOP NON-POLAR)
 
 purify_file() {
 	DNA_SEQ=`echo $(<$1) | tr "[:lower:]U" "[:upper:]T" | tr -d "\n" | sed "s/[^ATGC]*//g"`
-	sequence_check ${DNA_SEQ}
+	empty_check ${DNA_SEQ}
 }
 
 purify_console() {
 	DNA_SEQ=`echo "$1" | tr "[:lower:]U" "[:upper:]T" | tr -d "\n" | sed "s/[^ATGC]*//g"`
-	sequence_check ${DNA_SEQ}
+	empty_check ${DNA_SEQ}
 }
 
-sequence_check() {
+empty_check() {
 	if [ -z "$1" ]; then
 		echo -e "${red}ERROR${plain}: DNA sequence empty."
 		exit 1
 	fi
-	echo -e "DNA sequence: ${green}${DNA_SEQ}${plain}"
+	echo -e "DNA sequence:  ${green}${1}${plain}"
 	polypeptide ${1}
 }
 
+binding_site_p() {
+	FSEQ=`echo ${1} | tr "T" "U"`
+	SDSEQ="" && AUG="" && i=0 && ELSEQ=""
+	echo -e "mRNA sequence: ${yellow}${FSEQ}${plain}"
+	while [ $i -lt "${#FSEQ}" ]; do
+		if [ "${SDSEQ}" != "AGGAGG" ] && [ "${FSEQ:$i:1}" == "A" ] && [ "${FSEQ:$i+1:5}" == "GGAGG" ]; then
+			SDSEQ=`echo "${FSEQ:$i:1}${FSEQ:$i+1:5}"`
+		fi
+		if [ "${SDSEQ}" == "AGGAGG" ] && [ "${FSEQ:$i:1}" == "A" ] && [ "${FSEQ:$i+1:2}" == "UG" ]; then
+			AUG=`echo "${FSEQ:$i:1}${FSEQ:$i+1:2}"`
+		fi
+		if [ "${SDSEQ}" == "AGGAGG" ] && [ "${AUG}" == "AUG" ]; then
+			ELSEQ=`echo "${FSEQ:$i:$((${#FSEQ} - i))}"`
+			break
+		fi
+		i=$(($i + 1))
+	done
+	echo "Starting point of translation: ${ELSEQ}"
+}
+
+cd .g	
+
 polypeptide() {
-	if [ `expr ${#1} % 3` == 0 ]; then
-		echo -ne "Polypeptide: \n"
-		for ((i = 0; i < ${#1}; i+=3)); do
-			seq=`echo -e "${DNA_SEQ:$i:3}"`
-			for ((j = 0; j < 64; j++)); do
-				if [ "${seq}" == "${CODON[j]}" ]; then
-					echo -e "$((i/3+1)):\t${green}${CODON[j]}${plain}  ${bold}${AMINO[j]}${plain}  ${bold}${PROPT[j]}${plain}"
-				fi
-			done
-			sleep 0.01
-			if [ $(( $((i/3+1)) % 10 )) -eq 0 ]; then
-				echo -e "--------------$((i/3+1))--------------"
+	echo -ne "Polypeptide: \n"
+	for ((i = 0; i < ${#1}; i+=3)); do
+		seq=`echo -e "${DNA_SEQ:$i:3}"`
+		for ((j = 0; j < 64; j++)); do
+			if [ "${seq}" == "${CODON[j]}" ]; then
+				echo -e "$((i/3+1)):\t${green}${CODON[j]}${plain}  ${bold}${AMINO[j]}${plain}  ${PROPT[j]}"
 			fi
 		done
-		echo -e "Translation complete!"
-		exit 0
-	else
-		echo -e "${red}ERROR${plain}: DNA sequence invalid."
-	fi
+		sleep 0.01
+		if [ $(( $((i/3+1)) % 10 )) -eq 0 ]; then
+			echo -e "--------------$((i/3+1))--------------"
+		fi
+	done
+	echo -e "Translation complete!"
 }
 
 usage() {
@@ -69,12 +84,13 @@ usage() {
 ${PROGNAME}
 
 Usage: ${PROGNAME} [-f <path>] [-h] [-i <seq>] [-v]
--f:	translate a DNA sequence from a file
+-f:	translate a DNA/mRNA sequence from a file
+-i:	translate a DNA/mRNA sequence provided in console
+
 -h:	displays help message
--i:	translate a DNA sequence provided in console
 -v:	displays version
 
-Note: ${PROGNAME} translates DNA from 5' to 3'.
+Note: ${PROGNAME} reads the coding strand of dsDNA.
       ${PROGNAME} has mRNA capability.
 
 EOF
